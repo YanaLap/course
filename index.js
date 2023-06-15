ebb3450beb147ebeeeeedae9777af646de79eb2c
YanaLap <lap1kz@gmail.com>
2023-06-15 14:34:32 +0300

    feat: add hw2

--- index.js
+++ index.js
@@ -31,11 +31,12 @@
 
 // ДЗ 2
 
+
 // let number = +prompt('Введіть число, для отримання модуля')
 // if (number < 0) {
 //    alert('Ваше число' + ' ' + -number)
 // } else {
-//    alert('Ваше число' + ' ' + number)
+// alert('Ваше число' + ' ' + number)
 // }
 
 // let a = 5;
@@ -45,6 +46,50 @@
 // a *= 2;
 // console.log(a + b + c)
 
+// 1.
 // const temp = +prompt("Введіть температуру по Цельсію");
 // const formula = (9 / 5) * temp + 32;
 // alert(`Температура по Фаренгейту ${formula}`);
+
+// 2.
+// let name = "Василий";
+
+// let admin = name;
+// console.log(admin);
+
+// 3.
+
+// console.log(10 + 10 + "10") //2010
+// console.log(10 + "10" + 10)  //101010
+// console.log(10 + 10 + +"10") //30
+
+// console.log(10 / -"") //-infinity 
+
+// 4.
+
+// let sum = +prompt('Введіть сумму грошей у гаманці'); //21
+// let сhockoled = +prompt('Введіть ціну за шоколадку');  //5
+// let quantity = (sum - sum%сhockoled )/сhockoled
+
+// alert (`Ви можете купити ${quantity} шоколадок, i у вас залишиться ${sum%сhockoled} грошей`)
+
+// 6.
+
+// let number = +prompt('Введіть число, щоб дізнатися парне воно чи ні')
+
+// if (number % 2 === 0) {
+//    alert('Ваше число є парним')
+// } else {
+//    alert('Ваше число непарне')
+// }
+
+// 7.
+// let number = +prompt('Введіть трьохзначне число')
+// alert('Ваше число' + ' ' + (number % 10) + (Math.floor(number / 10) % 10) + (Math.floor(number / 100) % 10))
+
+
+
+
+
+
+
