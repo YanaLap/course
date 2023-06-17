7dddc6cc84a0df3f382516e858318e0efd0d61ff
YanaLap <lap1kz@gmail.com>
2023-06-17 15:47:00 +0300

    feat: add hw3

--- index.js
+++ index.js
@@ -87,9 +87,128 @@
 // let number = +prompt('Введіть трьохзначне число')
 // alert('Ваше число' + ' ' + (number % 10) + (Math.floor(number / 10) % 10) + (Math.floor(number / 100) % 10))
 
+// Дз 3
+
+// 1
+
+// let age = +prompt('Введіть свій вік');
+// if (age >= 0 && age <= 2) {
+//    alert('Ви дитина')
+// } else if (age >= 12 && age < 18) {
+//    alert('Ви підліток')
+// } else if (age >= 18 && age < 60) {
+//    alert('Ви дорослий')
+// } else if (age >= 60) {
+//    alert('Ви пенсіонер')
+// } else if (age < 0) {
+//    alert('Некоректно введені дані')
+// }else{
+//    alert('Ми не знаємо хто ви')
+// }
+
+// 2
+// let number = +prompt('Введіть число від 0 до 9, щоб дізнатися його спецсимвол')
+// switch (number) {
+//    case 0:
+//       alert('Ваш символ ")"')
+//       break;
+//    case 1:
+//       alert('Ваш символ "!"')
+//       break;
+//    case 2:
+//       alert('Ваш символ "@"')
+//       break;
+//    case 3:
+//       alert('Ваш символ "#"')
+//       break;
+//    case 4:
+//       alert('Ваш символ "$"')
+//       break;
+//    case 5:
+//       alert('Ваш символ "%"')
+//       break;
+//    case 6:
+//       alert('Ваш символ "^"')
+//       break;
+//    case 7:
+//       alert('Ваш символ "&"')
+//       break;
+//    case 8:
+//       alert('Ваш символ "*"')
+//       break;
+//    case 9:
+//       alert('Ваш символ "("')
+//       break;
+//    default: alert('Не вірно введені дані')
+// }
 
 
+// 3
+
+// let number = +prompt('Введіть 3-значне число. Цифри не мають повторюватись')
+
+// let a = Math.floor((number / 100) % 10);
+
+// let b = Math.floor((number / 10) % 10);
+// let c = number % 10;
+
+// if (a === b || a === c || b === c) {
+//    alert('У введеній цифрі є повтори')
+// } else {
+//    alert('Цифри не повторюються')
+// }
+
+// 4
+
+// let year = +prompt('Введіть рік')
+
+// let a = year % 400;
+// let b = year % 4;
+// let c = year % 100;
+
+// if (a === 0 || (b === 0 && c !== 0)) {
+//    alert('Цей рік в')
+// }else{
+//    alert('Звичайний рік')
+// }
 
 
+// 5
+// 1 дані
 
+let mMark = 78;
+let hMark = 1.69;
+let mDjon = 92;
+let hDjon = 1.95;
+
+let BMImark = (mMark / hMark ** 2).toFixed(2)
+let BMIdjon = (mDjon / hDjon ** 2).toFixed(2)
+let markHigherBMI = 'Марк имеет более высокий BMI'
+
+// 2 дані
+
+// mMark = 95;
+// hMark = 1.88;
+// mDjon = 85;
+// hDjon = 1.76;
+
+// BMImark = (mMark / hMark ** 2).toFixed(2)
+// BMIdjon = (mDjon / hDjon ** 2).toFixed(2)
+// let markHigherBMI = 'Джон имеет более высокий BMI'
+
+// 1
+
+if (BMImark > BMIdjon) {
+   console.log('BMI Марка выше, чем у Джона!');
+} else {
+   console.log('BMI Джона выше, чем у Марка!');
+}
+
+// 2
+
+// if (BMImark > BMIdjon) {
+//    console.log(`BMI Марка (${BMImark}) выше, чем у Джона (${BMIdjon})!`);
+// } else {
+//    console.log(`BMI Джона (${BMIdjon}) выше, чем у Марка (${BMImark})! `);
+// }
 
